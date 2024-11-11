8e64fa236c4ada466595174a26665e7d6f80bc44
JJ-Author <jj-author@users.noreply.github.com>
2024-11-11 23:32:45 +0000

    quick fix to make api accessible

--- API_main.py
+++ API_main.py
@@ -297,4 +297,4 @@ with get_weaviate_client() as client:
     
     # Run the Flask app
     if __name__ == '__main__':
-        app.run(host='127.0.0.1', port=8014, debug=True)
+        app.run(host='0.0.0.0', port=8014, debug=True)
